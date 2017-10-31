6685e21035953311066a6e7af37063b652ee9074
Brian McCann <Brian@Users-MBP.home>
2017-10-30 23:35:36 -0400

    starting solving data problems

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const data = require('./code/smallData')
+
+console.log(data.length)
+
+
+
+
+
+
+// const focusData = data.map( entry => {
+//    return [entry[0],entry[15], entry[29], entry[30]]
+// })
+
+// console.log(focusData.length)
+
+// console.log(focusData[3000])
+
+//   console.log('*************')
+// console.log(` id at index '0' ${entry[0]}`)
+
+// console.log(`crime-type at index '15' ${entry[15]}`)
+
+// console.log(`coordinate[s at 27 + 28 ${entry[29]}  ${entry[30]} `)
+// console.log('*************')
+
+
