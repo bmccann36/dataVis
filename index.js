0af1763f03bdc9663c29cb32b981e4ddd73dfa18
Brian McCann <Brian@Users-MBP.home>
2017-10-31 08:20:13 -0400

    played a little

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ console.log(data.length)
 
 
 
+const focusData = data.map( entry => {
+   return {
+     id: entry[0],
+     cat: entry[15],
+     lat: entry[29],
+     lon: entry[30]
+   }
+})
+
+console.log(focusData[100])
+
+// sort function
+// put smallest latitude first
+// if lat[a] = lat[b]  && lon[a] > lon[b] switch a with b
 
 
 
-// const focusData = data.map( entry => {
-//    return [entry[0],entry[15], entry[29], entry[30]]
-// })
-
-// console.log(focusData.length)
-
-// console.log(focusData[3000])
-
-//   console.log('*************')
-// console.log(` id at index '0' ${entry[0]}`)
-
-// console.log(`crime-type at index '15' ${entry[15]}`)
-
-// console.log(`coordinate[s at 27 + 28 ${entry[29]}  ${entry[30]} `)
-// console.log('*************')
 
 
